806771d59c0ff30db0fb008175efc9dc132ce814
Nathaniel Cook <nvcook42@gmail.com>
2023-05-01 10:07:22 -0600

    fix: use aws creds to login to ecr

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -6,10 +6,10 @@
 # Use:
 #
 #   export DOCKER_PASSWORD=$(aws ecr-public get-login-password --region us-east-1)
+#   echo "${DOCKER_PASSWORD}" | docker login --username AWS --password-stdin public.ecr.aws/r5b3e0r5
 #
-# to get a docker login password.
+# to login to docker. That password will be valid for 12h.
 
-echo "${DOCKER_PASSWORD}" | docker login --username AWS --password-stdin public.ecr.aws/r5b3e0r5
 docker buildx build -t 3box/ceramic-one .
 docker tag 3box/ceramic-one:latest public.ecr.aws/r5b3e0r5/3box/ceramic-one:latest
 docker push public.ecr.aws/r5b3e0r5/3box/ceramic-one:latest
