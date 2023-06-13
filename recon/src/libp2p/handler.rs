04457465eadec8925da5ce41bc3baff72b425013
Nathaniel Cook <nvcook42@gmail.com>
2023-06-13 13:41:19 -0600

    fix: add tests for recon libp2p

--- recon/src/libp2p/handler.rs
+++ recon/src/libp2p/handler.rs
@@ -3,6 +3,7 @@
 //! A handler is created for each connected peer that speaks the Recon protocol.
 //! A handler is responsible for performing Recon synchronization with a peer.
 use std::{
+    collections::VecDeque,
     task::Poll,
     time::{Duration, Instant},
 };
@@ -13,25 +14,40 @@ use libp2p::{
     futures::FutureExt,
     swarm::{
         handler::{FullyNegotiatedInbound, FullyNegotiatedOutbound},
-        ConnectionHandler, ConnectionHandlerEvent, KeepAlive, SubstreamProtocol,
+        ConnectionHandler, ConnectionHandlerEvent, ConnectionId, KeepAlive, SubstreamProtocol,
     },
 };
+use libp2p_identity::PeerId;
 use tracing::debug;
 
 use crate::libp2p::{protocol, Recon, PROTOCOL_NAME};
 
 pub struct Handler<R> {
-    state: State,
+    remote_peer_id: PeerId,
+    connection_id: ConnectionId,
     recon: R,
+    state: State,
+    keep_alive_duration: Duration,
     keep_alive: KeepAlive,
+    behavior_events_queue: VecDeque<FromHandler>,
 }
 
 impl<R> Handler<R> {
-    pub fn new(recon: R) -> Self {
+    pub fn new(
+        peer_id: PeerId,
+        connection_id: ConnectionId,
+        state: State,
+        keep_alive_duration: Duration,
+        recon: R,
+    ) -> Self {
         Self {
-            state: State::default(),
+            remote_peer_id: peer_id,
+            connection_id,
             recon,
-            keep_alive: KeepAlive::No,
+            state,
+            keep_alive_duration,
+            keep_alive: KeepAlive::Yes,
+            behavior_events_queue: VecDeque::new(),
         }
     }
     // Transition the state to a new state.
@@ -40,6 +56,8 @@ impl<R> Handler<R> {
     // should map to exactly one call of this transition_state function.
     fn transition_state(&mut self, state: State) {
         debug!(
+            %self.remote_peer_id,
+            ?self.connection_id,
             previous_state = ?self.state,
             new_state = ?state,
             "state transition"
@@ -48,8 +66,9 @@ impl<R> Handler<R> {
         // Update KeepAlive
         self.keep_alive = match (&self.state, self.keep_alive) {
             (State::Idle, k @ KeepAlive::Until(_)) => k,
-            (State::Idle, _) => KeepAlive::Until(Instant::now() + Duration::from_secs(2)),
+            (State::Idle, _) => KeepAlive::Until(Instant::now() + self.keep_alive_duration),
             (State::RequestOutbound, _)
+            | (State::WaitingInbound, _)
             | (State::WaitingOutbound, _)
             | (State::Outbound(_), _)
             | (State::Inbound(_), _) => KeepAlive::Yes,
@@ -63,20 +82,24 @@ type SyncFuture = libp2p::futures::future::BoxFuture<'static, Result<()>>;
 ///
 /// State Transitions:
 ///
-/// | State           | Event                                    | New State       |
-/// | -----           | -----                                    | ---------       |
-/// | Idle            | FromBehaviour::StartSync                 | RequestOutbound |
-/// | Idle            | ConnectionEvent::FullyNegotiatedInbound  | Inbound         |
-/// | RequestOutbound | poll                                     | WaitingOutbound |
-/// | WaitingOutbound | ConnectionEvent::FullyNegotiatedOutbound | Outbound        |
-/// | Outbound        | Poll::Ready (i.e. future completed)      | Idle            |
-/// | Inbound         | Poll::Ready (i.e. future completed)      | Idle            |
+/// | State            | Event                                    | New State       |
+/// | -----            | -----                                    | ---------       |
+/// | Idle             | FromBehaviour::StartSync                 | RequestOutbound |
+/// | Idle             | ConnectionEvent::FullyNegotiatedInbound  | Inbound         |
+/// | WaitingInbound*  | ConnectionEvent::FullyNegotiatedInbound  | Inbound         |
+/// | RequestOutbound* | poll                                     | WaitingOutbound |
+/// | WaitingOutbound  | ConnectionEvent::FullyNegotiatedOutbound | Outbound        |
+/// | WaitingOutbound  | ConnectionEvent::DialUpgradeError        | Idle            |
+/// | WaitingInbound   | ConnectionEvent::ListenUpgradeError      | Idle            |
+/// | Outbound         | Poll::Ready (i.e. future completed)      | Idle            |
+/// | Inbound          | Poll::Ready (i.e. future completed)      | Idle            |
 ///
-/// No other transitions are possible
-#[derive(Default)]
-enum State {
-    #[default]
+/// No other transitions are possible.
+///
+/// * Starting states
+pub enum State {
     Idle,
+    WaitingInbound,
     RequestOutbound,
     WaitingOutbound,
     Outbound(SyncFuture),
@@ -88,6 +111,7 @@ impl std::fmt::Debug for State {
         match self {
             Self::Idle => write!(f, "Idle"),
             Self::RequestOutbound => write!(f, "RequestOutbound"),
+            Self::WaitingInbound => write!(f, "WaitingInbound"),
             Self::WaitingOutbound => write!(f, "WaitingOutbound"),
             Self::Outbound(_) => f.debug_tuple("Outbound").field(&"_").finish(),
             Self::Inbound(_) => f.debug_tuple("Inbound").field(&"_").finish(),
@@ -101,35 +125,26 @@ pub enum FromBehaviour {
 }
 #[derive(Debug)]
 pub enum FromHandler {
-    PeerSynchronized,
+    Started,
+    Succeeded,
+    Stopped,
+    Failed(anyhow::Error),
 }
 
 #[derive(Debug)]
-pub enum Failure {
-    Timeout,
-    Unsupported,
-    Other {
-        error: Box<dyn std::error::Error + Send + 'static>,
-    },
+pub struct Failure {
+    error: Box<dyn std::error::Error + Send + 'static>,
 }
 
 impl std::fmt::Display for Failure {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Failure::Timeout => f.write_str("Ping timeout"),
-            Failure::Other { error } => write!(f, "Ping error: {error}"),
-            Failure::Unsupported => write!(f, "Ping protocol not supported"),
-        }
+        write!(f, "Error: {}", self.error)
     }
 }
 
 impl std::error::Error for Failure {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Failure::Timeout => None,
-            Failure::Unsupported => None,
-            Failure::Other { error } => Some(&**error),
-        }
+        Some(&*self.error)
     }
 }
 
@@ -149,6 +164,7 @@ impl<R: Recon + Clone + Send + 'static> ConnectionHandler for Handler<R> {
     }
 
     fn connection_keep_alive(&self) -> KeepAlive {
+        debug!(?self.keep_alive, "connection_keep_alive");
         self.keep_alive
     }
 
@@ -163,43 +179,30 @@ impl<R: Recon + Clone + Send + 'static> ConnectionHandler for Handler<R> {
             Self::Error,
         >,
     > {
+        if let Some(event) = self.behavior_events_queue.pop_back() {
+            return Poll::Ready(ConnectionHandlerEvent::Custom(event));
+        }
         match &mut self.state {
-            State::Idle => {}
+            State::Idle | State::WaitingOutbound | State::WaitingInbound => {}
             State::RequestOutbound => {
                 self.transition_state(State::WaitingOutbound);
                 // Start outbound connection
                 let protocol = SubstreamProtocol::new(ReadyUpgrade::new(PROTOCOL_NAME), ());
                 return Poll::Ready(ConnectionHandlerEvent::OutboundSubstreamRequest { protocol });
             }
-            State::WaitingOutbound => {}
-            State::Outbound(outbound) => {
-                if let Poll::Ready(result) = outbound.poll_unpin(cx) {
+            State::Outbound(stream) | State::Inbound(stream) => {
+                if let Poll::Ready(result) = stream.poll_unpin(cx) {
                     self.transition_state(State::Idle);
                     match result {
                         Ok(_) => {
                             return Poll::Ready(ConnectionHandlerEvent::Custom(
-                                FromHandler::PeerSynchronized,
+                                FromHandler::Succeeded,
                             ))
                         }
                         Err(e) => {
-                            // TODO handle error
-                            eprintln!("{e:?}")
-                        }
-                    }
-                }
-            }
-            State::Inbound(inbound) => {
-                if let Poll::Ready(result) = inbound.poll_unpin(cx) {
-                    self.transition_state(State::Idle);
-                    match result {
-                        Ok(_) => {
                             return Poll::Ready(ConnectionHandlerEvent::Custom(
-                                FromHandler::PeerSynchronized,
+                                FromHandler::Failed(e),
                             ))
-                        }
-                        Err(e) => {
-                            // TODO handle error
-                            eprintln!("{e:?}")
                         }
                     }
                 }
@@ -212,11 +215,12 @@ impl<R: Recon + Clone + Send + 'static> ConnectionHandler for Handler<R> {
     fn on_behaviour_event(&mut self, event: Self::InEvent) {
         match event {
             FromBehaviour::StartSync => {
-                debug!("start sync from behavior");
+                debug!(%self.remote_peer_id,  ?self.connection_id, "start sync from behavior");
                 match self.state {
                     State::Idle => self.transition_state(State::RequestOutbound),
                     State::RequestOutbound
                     | State::WaitingOutbound
+                    | State::WaitingInbound
                     | State::Outbound(_)
                     | State::Inbound(_) => {}
                 }
@@ -238,30 +242,92 @@ impl<R: Recon + Clone + Send + 'static> ConnectionHandler for Handler<R> {
                 FullyNegotiatedInbound {
                     protocol: stream, ..
                 },
-            ) => match self.state {
-                State::Idle => self.transition_state(State::Inbound(
-                    protocol::synchronize(self.recon.clone(), stream, false).boxed(),
-                )),
-                // Ignore inbound connection when we are not expecting it
-                State::RequestOutbound
-                | State::WaitingOutbound
-                | State::Outbound(_)
-                | State::Inbound(_) => {}
-            },
+            ) => {
+                debug!(%self.remote_peer_id, ?self.connection_id, "on_connection_event::FullyNegotiatedInbound");
+                match self.state {
+                    State::Idle | State::WaitingInbound => {
+                        self.behavior_events_queue.push_front(FromHandler::Started);
+                        self.transition_state(State::Inbound(
+                            protocol::synchronize(
+                                self.remote_peer_id,
+                                self.connection_id,
+                                self.recon.clone(),
+                                stream,
+                                false,
+                            )
+                            .boxed(),
+                        ));
+                    }
+                    // Ignore inbound connection when we are not expecting it
+                    State::RequestOutbound
+                    | State::WaitingOutbound
+                    | State::Inbound(_)
+                    | State::Outbound(_) => {}
+                }
+            }
             libp2p::swarm::handler::ConnectionEvent::FullyNegotiatedOutbound(
                 FullyNegotiatedOutbound {
                     protocol: stream, ..
                 },
-            ) => match self.state {
-                State::WaitingOutbound => self.transition_state(State::Outbound(
-                    protocol::synchronize(self.recon.clone(), stream, true).boxed(),
-                )),
-                // Ignore outbound connection when we are not expecting it
-                State::Idle | State::RequestOutbound | State::Outbound(_) | State::Inbound(_) => {}
-            },
+            ) => {
+                debug!(%self.remote_peer_id, ?self.connection_id, "on_connection_event::FullyNegotiatedOutbound");
+                match self.state {
+                    State::WaitingOutbound => {
+                        self.behavior_events_queue.push_front(FromHandler::Started);
+                        self.transition_state(State::Outbound(
+                            protocol::synchronize(
+                                self.remote_peer_id,
+                                self.connection_id,
+                                self.recon.clone(),
+                                stream,
+                                true,
+                            )
+                            .boxed(),
+                        ));
+                    }
+                    // Ignore outbound connection when we are not expecting it
+                    State::Idle
+                    | State::WaitingInbound
+                    | State::RequestOutbound
+                    | State::Outbound(_)
+                    | State::Inbound(_) => {}
+                }
+            }
             libp2p::swarm::handler::ConnectionEvent::AddressChange(_) => {}
-            libp2p::swarm::handler::ConnectionEvent::DialUpgradeError(_) => {}
-            libp2p::swarm::handler::ConnectionEvent::ListenUpgradeError(_) => {}
+            // We failed to upgrade the inbound connection.
+            libp2p::swarm::handler::ConnectionEvent::ListenUpgradeError(_) => {
+                debug!(%self.remote_peer_id, ?self.connection_id, "on_connection_event::ListenUpgradeError");
+                match self.state {
+                    State::WaitingInbound => {
+                        // We have stopped synchronization and cannot attempt again as we are unable to
+                        // negotiate a protocol.
+                        self.behavior_events_queue.push_front(FromHandler::Stopped);
+                        self.transition_state(State::Idle)
+                    }
+                    State::Idle
+                    | State::WaitingOutbound
+                    | State::RequestOutbound
+                    | State::Outbound(_)
+                    | State::Inbound(_) => {}
+                }
+            }
+            // We failed to upgrade the outbound connection.
+            libp2p::swarm::handler::ConnectionEvent::DialUpgradeError(_) => {
+                debug!(%self.remote_peer_id, ?self.connection_id, "on_connection_event::DialUpgradeError");
+                match self.state {
+                    State::WaitingOutbound => {
+                        // We have stopped synchronization and cannot attempt again as we are unable to
+                        // negotiate a protocol.
+                        self.behavior_events_queue.push_front(FromHandler::Stopped);
+                        self.transition_state(State::Idle)
+                    }
+                    State::Idle
+                    | State::WaitingInbound
+                    | State::RequestOutbound
+                    | State::Outbound(_)
+                    | State::Inbound(_) => {}
+                }
+            }
         }
     }
 }
