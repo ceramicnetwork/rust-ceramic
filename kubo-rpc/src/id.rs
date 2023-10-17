a24146bb761bfca9413564d46ae4c4f567f37e9f
Nathaniel Cook <nvcook42@gmail.com>
2023-10-17 12:23:26 -0600

    fix: append peer id to all listen addrs
    This change follows Kubo's implementation and is helpful for clients as
    then can simply iterate through the list of addrs when attempt to peer
    with them instead of having to construct the full mulitaddr themselves.

--- kubo-rpc/src/id.rs
+++ kubo-rpc/src/id.rs
@@ -1,4 +1,7 @@
 //! Provides methods for looking up peer info.
+
+use multihash::Multihash;
+
 use crate::{error::Error, IpfsDep, PeerId, PeerInfo};
 
 /// Lookup information about a specific peer.
@@ -7,12 +10,32 @@ pub async fn lookup<T>(client: T, peer_id: PeerId) -> Result<PeerInfo, Error>
 where
     T: IpfsDep,
 {
-    client.lookup(peer_id).await
+    Ok(add_peer_id_to_addrs(client.lookup(peer_id).await?))
 }
 /// Lookup information about the local peer.
 pub async fn lookup_local<T>(client: T) -> Result<PeerInfo, Error>
 where
     T: IpfsDep,
 {
-    client.lookup_local().await
+    Ok(add_peer_id_to_addrs(client.lookup_local().await?))
+}
+
+// Adds the peer's own peer ID to its listen_addrs if it's not already present.
+fn add_peer_id_to_addrs(mut peer: PeerInfo) -> PeerInfo {
+    let peer_mh: Multihash = peer.peer_id.into();
+    peer.listen_addrs = peer
+        .listen_addrs
+        .into_iter()
+        .map(|addr| {
+            if !addr
+                .iter()
+                .any(|addr| matches!(addr, multiaddr::Protocol::P2p(_)))
+            {
+                addr.with(multiaddr::Protocol::P2p(peer_mh))
+            } else {
+                addr
+            }
+        })
+        .collect();
+    peer
 }
