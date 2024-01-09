e70e2e89dd58449d31b25a61f9cc696428e7ccff
Ben Wilson <ben@3box.io>
2024-01-09 16:33:01 -0500

    feat: schedule_k8s_deploy github action
    make script executable
    
    Added env to deploy-image
    
    use gh_environment
    
    quoted environment
    
    fix: editorial
    
    fix: editorial
    
    uncomment ecs job and default to main branch for schedule_k8s_deploy

---
+++ ci-scripts/schedule_k8s_deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+id=$(uuidgen)
+job_id=$(uuidgen)
+now=$(date +%s%N)
+ttl=$(date +%s -d "14 days")
+image=public.ecr.aws/r5b3e0r5/3box/ceramic-one:${2-latest}
+branch=${TEST_BRANCH-main}
+network=${1-dev}
+environment=ceramic-v4-${network}
+
+docker run --rm -i \
+  -e "AWS_REGION=$AWS_REGION" \
+  -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+  -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
+  -v ~/.aws:/root/.aws \
+  -v "$PWD":/aws \
+  amazon/aws-cli dynamodb put-item --table-name "ceramic-$network-ops" --item \
+  "{                                                        \
+    \"id\":     {\"S\": \"$id\"},                           \
+    \"job\":    {\"S\": \"$job_id\"},                       \
+    \"ts\":     {\"N\": \"$now\"},                          \
+    \"ttl\":    {\"N\": \"$ttl\"},                          \
+    \"stage\":  {\"S\": \"queued\"},                        \
+    \"type\":   {\"S\": \"workflow\"},                      \
+    \"params\": {                                           \
+      \"M\": {                                              \
+        \"name\":     {\"S\": \"Deploy CERAMIC ONE\"},      \
+        \"org\":      {\"S\": \"3box\"},                    \
+        \"repo\":     {\"S\": \"ceramic-infra\"},           \
+        \"ref\":      {\"S\": \"$branch\"},                 \
+        \"workflow\": {\"S\": \"update_image.yml\"},        \
+        \"inputs\":   {                                     \
+          \"M\": {                                          \
+            \"ceramic_one_image\": {\"S\": \"$image\"},     \
+            \"environment\": {\"S\": \"$environment\"}      \
+          }                                                 \
+        }                                                   \
+      }                                                     \
+    }                                                       \
+  }"
