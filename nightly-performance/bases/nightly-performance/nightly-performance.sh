c37cf47e5dbad2a4d554adea663967fc294a4ba2
Ben Wilson <ben@3box.io>
2024-02-15 12:37:56 -0500

    feat: added dev-hermetic-amd

--- nightly-performance/bases/nightly-performance/nightly-performance.sh
+++ nightly-performance/bases/nightly-performance/nightly-performance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-SIM_NAME=${JOB_NAMESPACE}-$(date +%Y-%m-%d-%H-%M)
+SIM_NAME=${JOB_NAMESPACE}-$(date +%Y-%m-%d-%H)
 export SIM_NAME
-NETWORK_NAME=${JOB_NAMESPACE}-$(date +%Y-%m-%d-%H-%M)
+NETWORK_NAME=${JOB_NAMESPACE}-$(date +%Y-%m-%d-%H)
 export NETWORK_NAME
 NETWORK_NAMESPACE=keramik-${NETWORK_NAME}
 export NETWORK_NAMESPACE
@@ -11,11 +11,25 @@ export NETWORK_NAMESPACE
 curl -L https://github.com/mikefarah/yq/releases/download/v4.40.7/yq_linux_amd64 -o yq
 chmod +x yq
 
+# Customize the network name
 ./yq -e '.metadata.name = env(NETWORK_NAME)' /config/network.yaml > network.yaml
-kubectl apply -f network.yaml
 
+cat network.yaml
+
+# If an affintiy file exists, apply it to the network.yaml
+if [ -n "$AFFINITY_FILE" ] && [ -f "$AFFINITY_FILE" ]; then
+  ./yq eval '.spec += load(env(AFFINITY_FILE))' network.yaml > network-merged.yaml
+else
+  cp network.yaml network-merged.yaml
+fi
+
+cat network-merged.yaml
+
+# Apply the network
+kubectl apply -f network-merged.yaml
 kubectl apply -n "${NETWORK_NAMESPACE}" -f /config/podmonitors.yaml
 
+# Wait for CAS to be ready
 while true; do
   READY_REPLICAS=$(kubectl get statefulset cas -n "${NETWORK_NAMESPACE}" -o jsonpath='{.status.readyReplicas}')
   if [[ "$READY_REPLICAS" -ge 1 ]]; then
@@ -26,6 +40,17 @@ while true; do
     sleep 10
   fi
 done
+
+# Label the workload with a storageClass, if it exists
+STORAGECLASS_NAME=$(./yq '.spec.ceramic[0].ipfs.rust.storageClass' network-merged.yaml)
+if [ -n "$STORAGECLASS_NAME" ]; then
+  kubectl label pods -l app=ceramic storageClass="$STORAGECLASS_NAME" -n "${NETWORK_NAMESPACE}"
+fi
+
+# Label the workload with an affinity tag, if it exists
+if [ -n "$AFFINITY_TAG" ]; then
+  kubectl label pods -l app=ceramic affinity="$AFFINITY_TAG" -n "${NETWORK_NAMESPACE}"
+fi
 
 sleep 300 # wait for the network to stabilize and bootstrap
 kubectl wait --for=condition=complete job/bootstrap -n "${NETWORK_NAMESPACE}"
