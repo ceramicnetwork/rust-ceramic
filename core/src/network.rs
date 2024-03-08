6dbe583278d8a19b5e9f54bd410b8ac52cce6284
Nathaniel Cook <nvcook42@gmail.com>
2024-03-08 13:48:38 -0700

    feat: report error if event network does not match
    Prior to this change events written via the API that do not match the
    configured network of the node were stored but never synchronized as
    interests are specific to the configured network.
    
    Now an error is returned from the API if the network of the event does
    not match the configured network.
    
    NOTE: This will eventually be unneeded logic once event ids are removed
    from the API.

--- core/src/network.rs
+++ core/src/network.rs
@@ -1,6 +1,14 @@
+use anyhow::{anyhow, Result};
+
+const MAINNET: u64 = 0x00;
+const TESTNET_CLAY: u64 = 0x01;
+const DEV_UNSTABLE: u64 = 0x02;
+const IN_MEMORY: u64 = 0xff;
+const LOCAL_OFFSET: u64 = 0x01_0000_0000;
+
 /// Network values from https://cips.ceramic.network/tables/networkIds.csv
 /// Ceramic Pubsub Topic, Timestamp Authority
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Network {
     /// Production network
     Mainnet,
@@ -16,14 +24,25 @@ pub enum Network {
 
 impl Network {
     /// Get the network as an integer id.
+    pub fn try_from_id(id: u64) -> Result<Self> {
+        match id {
+            MAINNET => Ok(Network::Mainnet),
+            TESTNET_CLAY => Ok(Network::TestnetClay),
+            DEV_UNSTABLE => Ok(Network::DevUnstable),
+            IN_MEMORY => Ok(Network::InMemory),
+            id if id > LOCAL_OFFSET => Ok(Network::Local((id - LOCAL_OFFSET) as u32)),
+            id => Err(anyhow!("unknown network id: {}", id)),
+        }
+    }
+    /// Get the network as an integer id.
     pub fn id(&self) -> u64 {
         match self {
             // https://github.com/ceramicnetwork/CIPs/blob/main/tables/networkIds.csv
-            Network::Mainnet => 0x00,
-            Network::TestnetClay => 0x01,
-            Network::DevUnstable => 0x02,
-            Network::InMemory => 0xff,
-            Network::Local(id) => 0x01_0000_0000_u64 + u64::from(*id),
+            Network::Mainnet => MAINNET,
+            Network::TestnetClay => TESTNET_CLAY,
+            Network::DevUnstable => DEV_UNSTABLE,
+            Network::InMemory => IN_MEMORY,
+            Network::Local(id) => LOCAL_OFFSET + *id as u64,
         }
     }
 
@@ -36,5 +55,10 @@ impl Network {
             Network::Local(i) => format!("/ceramic/local-{}", i),
             Network::InMemory => "/ceramic/inmemory".to_owned(),
         }
+    }
+}
+impl std::fmt::Display for Network {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name())
     }
 }
