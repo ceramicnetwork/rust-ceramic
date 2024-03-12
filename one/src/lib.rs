dc156b8c8e4801a7975977e1bbc1f055f2f8e480
Nathaniel Cook <nvcook42@gmail.com>
2024-03-12 14:20:06 -0600

    feat: add rust-ceramic clay bootstrap nodes

--- one/src/lib.rs
+++ one/src/lib.rs
@@ -248,6 +248,8 @@ impl Network {
                 "/dns4/go-ipfs-ceramic-public-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmWiY3CbNawZjWnHXx3p3DXsg21pZYTj4CRY1iwMkhP8r3".to_string(), // cspell:disable-line
                 "/dns4/go-ipfs-ceramic-private-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmQotCKxiMWt935TyCBFTN23jaivxwrZ3uD58wNxeg5npi".to_string(), // cspell:disable-line
                 "/dns4/go-ipfs-ceramic-private-cas-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmbeBTzSccH8xYottaYeyVX8QsKyox1ExfRx7T1iBqRyCd".to_string(), // cspell:disable-line
+                "/dns/rust-ceramic-v4-tnet-0.3box.io/tcp/4001/p2p/12D3KooWNYomhBwgoCZ5sbhkCfEmmHV8m3bvESm6PL1bjDU7H3ja/p2p/12D3KooWNYomhBwgoCZ5sbhkCfEmmHV8m3bvESm6PL1bjDU7H3ja".to_string(),// cspell:disable-line
+                "/dns/rust-ceramic-v4-tnet-1.3box.io/tcp/4001/p2p/12D3KooWPYNbc6VBfPuJQ84sNb4xNXysD383ZXZuLYbG8xTmHmfj/p2p/12D3KooWPYNbc6VBfPuJQ84sNb4xNXysD383ZXZuLYbG8xTmHmfj".to_string(), // cspell:disable-line
             ],
             Network::DevUnstable => vec![
                 "/dns4/go-ipfs-ceramic-public-qa-external.3boxlabs.com/tcp/4011/ws/p2p/QmPP3RdaSWDkhcxZReGo591FWanLw9ucvgmUZhtSLt9t6D".to_string(),  // cspell:disable-line
