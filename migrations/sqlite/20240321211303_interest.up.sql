108cf7d0256a15de465b65725743bcfb3b9b35b9
David Estes <david@3box.io>
2024-04-01 20:51:00 -0600

    feat: update interest to use migraitons and postgres param binding

--- migrations/sqlite/20240321211303_interest.up.sql
+++ migrations/sqlite/20240321211303_interest.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 
 CREATE TABLE IF NOT EXISTS interest (
-    id BLOB NOT NULL, -- network_id sort_value controller StreamID event_cid
+    order_key BLOB NOT NULL, -- network_id sort_value controller StreamID event_cid
     ahash_0 INTEGER NOT NULL, -- the ahash is decomposed as [u32; 8]
     ahash_1 INTEGER NOT NULL,
     ahash_2 INTEGER NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS interest (
     ahash_5 INTEGER NOT NULL,
     ahash_6 INTEGER NOT NULL,
     ahash_7 INTEGER NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(order_key)
 );
 
-SELECT id, ahash_0, ahash_1, ahash_2, ahash_3, ahash_4, ahash_5, ahash_6, ahash_7 FROM interest WHERE false;
+SELECT order_key, ahash_0, ahash_1, ahash_2, ahash_3, ahash_4, ahash_5, ahash_6, ahash_7 FROM interest WHERE false;
