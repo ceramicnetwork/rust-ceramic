b2e3a60eb726dcf1c6489e76e78ef883cdb67294
Nathaniel Cook <nvcook42@gmail.com>
2024-05-07 16:14:30 -0600

    fix: parse unsigned init events

--- api/src/server/event.rs
+++ api/src/server/event.rs
@@ -34,9 +34,13 @@ where
         car_blocks.insert(cid, bytes);
     }
     let event_bytes = get_block(&event_cid, &car_blocks, store).await?;
-    let event: unvalidated::Event =
+    let event: unvalidated::Event<Ipld> =
         serde_ipld_dagcbor::from_slice(&event_bytes).context("decoding event")?;
-    let init_id = match event {
+    let (init_id, payload) = match event {
+        unvalidated::Event::Time(event) => (
+            event.id(),
+            get_init_event_payload(&event.id(), &car_blocks, store).await?,
+        ),
         unvalidated::Event::Signed(event) => {
             let link = event
                 .link()
@@ -45,38 +49,50 @@ where
             let payload_bytes = get_block(&link, &car_blocks, store).await?;
             let payload: unvalidated::Payload<Ipld> =
                 serde_ipld_dagcbor::from_slice(&payload_bytes).context("decoding payload")?;
-            match payload {
+            let init_id = match payload {
                 unvalidated::Payload::Init(_) => event_cid,
                 unvalidated::Payload::Data(payload) => payload.id(),
-            }
+            };
+            (
+                init_id,
+                get_init_event_payload(&init_id, &car_blocks, store).await?,
+            )
         }
-        unvalidated::Event::Time(event) => event.id(),
+        unvalidated::Event::Unsigned(event) => (event_cid, event),
     };
 
-    let init_bytes = get_block(&init_id, &car_blocks, store).await?;
-    let init_event: unvalidated::Event =
+    Ok(EventId::new(
+        &network,
+        payload.header().sep(),
+        payload.header().model().as_slice(),
+        payload
+            .header()
+            .controllers()
+            .first()
+            .ok_or_else(|| anyhow!("init header should contain at least one controller"))?,
+        &init_id,
+        &event_cid,
+    ))
+}
+
+async fn get_init_event_payload(
+    init_id: &Cid,
+    car_blocks: &HashMap<Cid, Vec<u8>>,
+    store: &impl AccessModelStore,
+) -> Result<unvalidated::InitPayload<Ipld>> {
+    let init_bytes = get_block(init_id, car_blocks, store).await?;
+    let init_event: unvalidated::Event<Ipld> =
         serde_ipld_dagcbor::from_slice(&init_bytes).context("decoding init event")?;
     if let unvalidated::Event::Signed(event) = init_event {
         let link = event
             .link()
             .ok_or_else(|| anyhow!("init event should have a link"))?;
 
-        let payload_bytes = get_block(&link, &car_blocks, store).await?;
+        let payload_bytes = get_block(&link, car_blocks, store).await?;
         let payload: unvalidated::Payload<Ipld> =
             serde_ipld_dagcbor::from_slice(&payload_bytes).context("decoding init payload")?;
         if let unvalidated::Payload::Init(payload) = payload {
-            Ok(EventId::new(
-                &network,
-                payload.header().sep(),
-                payload.header().model().as_slice(),
-                payload
-                    .header()
-                    .controllers()
-                    .first()
-                    .ok_or_else(|| anyhow!("init header should contain at least one controller"))?,
-                &init_id,
-                &event_cid,
-            ))
+            Ok(payload)
         } else {
             bail!("init event payload is not well formed")
         }
@@ -232,7 +248,7 @@ mod tests {
     }
     #[tokio::test]
     #[traced_test]
-    async fn event_id_from_car_init_event() {
+    async fn event_id_from_car_signed_init_event() {
         let expected = expect![[r#"
             Ok(
                 EventId {
@@ -257,6 +273,46 @@ mod tests {
         "#]];
         // Init events do not need to access the store
         test_event_id_from_car(INIT_EVENT_CAR, expected, MockAccessModelStoreTest::new()).await
+    }
+
+    #[tokio::test]
+    #[traced_test]
+    async fn event_id_from_car_unsigned_init_event() {
+        let expected = expect![[r#"
+            Ok(
+                EventId {
+                    bytes: "ce0105004bf23f697fcdb44763a8eb5b47190f472416a164017112203130e432a07501b157851b7cc2e30ca2baed822009e02323daf04e4f2416a164",
+                    network_id: Some(
+                        0,
+                    ),
+                    separator: Some(
+                        "4bf23f697fcdb447",
+                    ),
+                    controller: Some(
+                        "63a8eb5b47190f47",
+                    ),
+                    stream_id: Some(
+                        "2416a164",
+                    ),
+                    cid: Some(
+                        "bafyreibrgdsdfidvagyvpbi3ptbogdfcxlwyeiaj4arshwxqjzhsifvbmq",
+                    ),
+                },
+            )
+        "#]];
+        test_event_id_from_car(
+            // Unsigned init payload event
+            "
+            uOqJlcm9vdHOB2CpYJQABcRIgMTDkMqB1AbFXhRt8wuMMorrtgiAJ4CMj2vBOTyQWoWRndmVyc2lvbgG
+            0AQFxEiAxMOQyoHUBsVeFG3zC4wyiuu2CIAngIyPa8E5PJBahZKJkZGF0YfZmaGVhZGVyo2NzZXBlbW9
+            kZWxlbW9kZWxYKM4BAgGFARIgV1HMaNjwORnyUJjowofErLoZ5HkFm-5dsy3v2onQm6NrY29udHJvbGx
+            lcnOBeDhkaWQ6a2V5Ono2TWt0Q0ZSY3dMUkZRQTlXYmVEUk03VzdrYkJkWlRIUTJ4blBneXhaTHExZ0N
+            wSw",
+            expected,
+            // Init events do not need to access the store
+            MockAccessModelStoreTest::new(),
+        )
+        .await
     }
 
     #[tokio::test]
