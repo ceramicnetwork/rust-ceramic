d754ae78a69d0453212c4b83b1093c3519b51df4
David Estes <david@3box.io>
2024-05-16 23:45:30 -0600

    fix: use channel to notify task of new events
    removed interval background job to find missed events. process them at startup. will come back to handing the overloaded edge case and dropping messages.
    
    still need to limit/remove the data allowed in memory. this tries to avoid building on the new event stuff, but is untested as such.

--- service/src/event/service.rs
+++ service/src/event/service.rs
@@ -1,15 +1,15 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use ceramic_core::EventId;
 use ceramic_event::unvalidated;
 use ceramic_store::{CeramicOneEvent, EventInsertable, EventInsertableBody, SqlitePool};
 use ipld_core::ipld::Ipld;
 use recon::InsertResult;
-use tracing::warn;
+use tracing::{trace, warn};
 
 use super::ordering_task::{
-    DeliverableEvent, DeliverableMetadata, DeliverableTask, OrderingState, OrderingTask,
-    StreamEvents,
+    DeliverableEvent, DeliverableMetadata, DeliverableTask, DeliveredEvent, OrderingState,
+    OrderingTask, StreamEvents,
 };
 
 use crate::{Error, Result};
@@ -65,6 +65,7 @@ impl CeramicEventService {
         let insertable = EventInsertableBody::try_from_carfile(event_cid, carfile).await?;
         let ev_block = insertable.block_for_cid(&insertable.cid)?;
 
+        trace!(count=%insertable.blocks.len(), cid=%event_cid, "parsing event blocks");
         let event_ipld: unvalidated::Event<Ipld> = serde_ipld_dagcbor::from_slice(&ev_block.bytes)
             .map_err(|e| {
                 Error::new_invalid_arg(
@@ -108,6 +109,7 @@ impl CeramicEventService {
         Ok((insertable, meta))
     }
 
+    #[tracing::instrument(skip(self, items), level = tracing::Level::DEBUG, fields(items = items.len()))]
     pub(crate) async fn insert_events_from_carfiles<'a>(
         &self,
         items: &[recon::ReconItem<'a, EventId>],
@@ -118,12 +120,14 @@ impl CeramicEventService {
         }
 
         let mut insert_now = Vec::with_capacity(items.len());
+        let mut notify_task_new = Vec::with_capacity(items.len());
         let mut insert_after_history_check = if must_have_history {
             Vec::with_capacity(items.len())
         } else {
             Vec::default()
         };
-        let mut check_deliverable_later = HashMap::new();
+
+        let mut background_task_deliverable = HashMap::new();
         for item in items {
             let cid = item.key.cid().ok_or_else(|| {
                 Error::new_invalid_arg(anyhow::anyhow!("EventID is missing a CID: {}", item.key))
@@ -136,52 +140,42 @@ impl CeramicEventService {
                 if must_have_history {
                     insert_after_history_check.push((meta, insertable));
                 } else {
-                    check_deliverable_later.insert(insertable.body.cid, meta);
+                    background_task_deliverable.insert(insertable.body.cid, meta);
                     insert_now.push(insertable);
                 }
             } else {
-                insertable.body.deliverable = true;
+                insertable.deliverable(true);
+                notify_task_new.push(DeliveredEvent::new(
+                    insertable.body.cid,
+                    insertable.body.cid,
+                ));
                 insert_now.push(insertable);
             }
         }
 
         if !insert_after_history_check.is_empty() {
-            // ideally, this map would be per stream, but we are just processing all of them together for now
-            let mut stream_map = StreamEvents::new();
-
-            insert_after_history_check.iter().for_each(|(meta, ev)| {
-                let _new =
-                    stream_map.add_event(DeliverableEvent::new(ev.body.cid, meta.to_owned(), None));
-            });
-
-            let deliverable =
-                OrderingState::discover_deliverable_events(&self.pool, &mut stream_map).await?;
-            if deliverable.len() != insert_after_history_check.len() {
-                let missing = insert_after_history_check
-                    .iter()
-                    .filter_map(|(_, ev)| {
-                        if !deliverable.contains(&ev.body.cid) {
-                            Some(ev.body.cid)
-                        } else {
-                            None
-                        }
-                    })
-                    .collect::<Vec<_>>();
-
-                return Err(Error::new_app(anyhow::anyhow!(
-                    "Missing required `prev` event CIDs: {:?}",
-                    missing
-                )));
-            } else {
-                for (_, mut insertable) in insert_after_history_check {
-                    insertable.body.deliverable = true;
-                    insert_now.push(insertable);
-                }
-            }
+            let deliverable = insert_now
+                .iter()
+                .filter_map(|e| {
+                    if e.body.deliverable {
+                        Some(e.body.cid)
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+            self.verify_history_inline(
+                insert_after_history_check,
+                &mut insert_now,
+                &mut notify_task_new,
+                &deliverable,
+            )
+            .await?;
         }
         let res = CeramicOneEvent::insert_many(&self.pool, &insert_now[..]).await?;
 
-        for ev in check_deliverable_later {
+        for ev in background_task_deliverable {
+            trace!(cid=%ev.0, prev=%ev.1.prev, init=%ev.1.init_cid, "sending to delivery task");
             if let Err(e) = self
                 .delivery_task
                 .tx
@@ -200,7 +194,87 @@ impl CeramicEventService {
                 }
             }
         }
+        for new in notify_task_new {
+            if let Err(e) = self.delivery_task.tx_new.try_send(new) {
+                match e {
+                    tokio::sync::mpsc::error::TrySendError::Full(ev) => {
+                        // we should only be doing this during recon, in which case we can rediscover events.
+                        // the delivery task will start picking up these events once it's drained since they are stored in the db
+                        warn!(attempt=?ev, limit=%PENDING_DELIVERABLE_EVENTS, "Notify new task full");
+                    }
+                    tokio::sync::mpsc::error::TrySendError::Closed(_) => {
+                        warn!("Delivery task closed. shutting down");
+                        return Err(Error::new_fatal(anyhow::anyhow!("Delivery task closed")));
+                    }
+                }
+            }
+        }
 
         Ok(res)
+    }
+
+    async fn verify_history_inline(
+        &self,
+        insert_after_history_check: Vec<(DeliverableMetadata, EventInsertable)>,
+        insert_now: &mut Vec<EventInsertable>,
+        notify_new: &mut Vec<DeliveredEvent>,
+        incoming_deliverable_cids: &HashSet<cid::Cid>,
+    ) -> Result<()> {
+        // ideally, this map would be per stream, but we are just processing all of them together for now
+        let mut to_check_map = StreamEvents::new();
+
+        let required_to_find = insert_after_history_check.len();
+        let mut found_in_batch = 0;
+        let mut insert_if_greenlit = Vec::with_capacity(required_to_find);
+
+        for (meta, mut ev) in insert_after_history_check {
+            if incoming_deliverable_cids.contains(&meta.prev) {
+                trace!(new=%ev.body.cid, prev=%meta.prev, "prev event being added in same batch");
+                found_in_batch += 1;
+                ev.deliverable(true);
+                notify_new.push(DeliveredEvent::new(ev.body.cid, meta.init_cid));
+                insert_now.push(ev);
+            } else {
+                let _new = to_check_map.add_event(DeliverableEvent::new(
+                    ev.body.cid,
+                    meta.to_owned(),
+                    None,
+                ));
+                insert_if_greenlit.push((meta, ev));
+            }
+        }
+
+        if to_check_map.is_empty() {
+            return Ok(());
+        }
+
+        let deliverable =
+            OrderingState::discover_deliverable_events(&self.pool, &mut to_check_map).await?;
+        if deliverable.len() != required_to_find - found_in_batch {
+            let missing = insert_if_greenlit
+                .iter()
+                .filter_map(|(_, ev)| {
+                    if !deliverable.contains(&ev.body.cid) {
+                        Some(ev.body.cid)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            tracing::info!(?missing, ?deliverable, "Missing required `prev` event CIDs");
+
+            Err(Error::new_invalid_arg(anyhow::anyhow!(
+                "Missing required `prev` event CIDs: {:?}",
+                missing
+            )))
+        } else {
+            for (meta, mut insertable) in insert_if_greenlit.drain(..) {
+                insertable.deliverable(true);
+                notify_new.push(DeliveredEvent::new(insertable.body.cid, meta.init_cid));
+                insert_now.push(insertable);
+            }
+            Ok(())
+        }
     }
 }
