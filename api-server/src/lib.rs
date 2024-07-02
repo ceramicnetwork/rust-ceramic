3d3b044b7d819aa5f7c7140b7e7f66404ac955b2
Github Automation <github@3box.io>
2024-07-02 00:57:14 +0000

    chore: version v0.26.0

--- api-server/src/lib.rs
+++ api-server/src/lib.rs
@@ -20,7 +20,7 @@ use swagger::{ApiError, ContextWrapper};
 type ServiceError = Box<dyn Error + Send + Sync + 'static>;
 
 pub const BASE_PATH: &str = "/ceramic";
-pub const API_VERSION: &str = "0.25.0";
+pub const API_VERSION: &str = "0.26.0";
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[must_use]
