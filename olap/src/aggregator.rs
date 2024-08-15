5ce34785e2d0a16f1c5a82314bd5c313a7f08c55
Nathaniel Cook <nvcook42@gmail.com>
2024-08-15 14:17:41 -0600

    fix: use datafusion

--- olap/src/aggregator.rs
+++ olap/src/aggregator.rs
@@ -1,8 +1,12 @@
 use std::future::Future;
 
 use anyhow::Result;
+use datafusion::execution::context::SessionContext;
 
 pub async fn run(shutdown_signal: impl Future<Output = ()>) -> Result<()> {
+    // Create datafusion context
+    let _ctx = SessionContext::new();
+
     shutdown_signal.await;
     Ok(())
 }
