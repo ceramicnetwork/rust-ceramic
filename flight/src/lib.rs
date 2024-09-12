6caebb2f204d3647d269750fab33de409152df79
Nathaniel Cook <nvcook42@gmail.com>
2024-09-12 10:35:30 -0600

    feat: add flight sql server to expose conclusion feed. (#523)
    The Flight SQL Server allows using SQL to query the conclusion feed.
    Events are read from the db and transformed into conclusion events and
    returned out the feed.

--- flight/src/lib.rs
+++ flight/src/lib.rs
@@ -1,8 +1,10 @@
+//! Implementation of a FlightSQL server for exposing Ceramic data.
+#![warn(missing_docs)]
+
 mod conversion;
 mod types;
 
-#[cfg(test)]
-mod tests;
+pub mod server;
 
 pub use conversion::*;
 pub use types::*;
--- flight/src/types.rs
+++ flight/src/types.rs
@@ -4,9 +4,16 @@ use cid::Cid;
 use ipld_core::ipld::Ipld;
 use serde::{Deserialize, Serialize};
 
+/// A Ceramic event annotated with conclusions about the event.
+///
+/// Conclusions included for all events:
+///     1. An event's signature has been verified
+///     2. An event's previous events will have an index less than the event's index.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum ConclusionEvent {
+    /// An event that contains data for a stream.
     Data(ConclusionData),
+    /// An event that contains temporal information for a stream.
     Time(ConclusionTime),
 }
 
@@ -17,7 +24,7 @@ impl AsRef<ConclusionEvent> for ConclusionEvent {
 }
 
 impl ConclusionEvent {
-    pub fn event_type_as_int(&self) -> u8 {
+    pub(crate) fn event_type_as_int(&self) -> u8 {
         match self {
             ConclusionEvent::Data(_) => 0,
             ConclusionEvent::Time(_) => 1,
@@ -25,33 +32,50 @@ impl ConclusionEvent {
     }
 }
 
-// Dimension is a tuple of (name, value) : way to identify a stream
+/// ConclusionInit is static metadata about a stream.
+/// All events within a stream have the same ConclusionInit.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ConclusionInit {
+    /// The CID of the init event of the stream. Can be used as a unique identifier for the stream.
+    /// This is not the StreamId as it does not contain the StreamType.
     pub stream_cid: Cid,
+    /// DID controller of the stream.
     pub controller: String,
+    /// Order set of key value pairs that annotate the stream.
     pub dimensions: Vec<(String, Vec<u8>)>,
 }
 
+/// ConclusionData represents a Ceramic event that contained data.
+///
+/// Additionally we have concluded to which stream the event belongs and its associated metadata.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ConclusionData {
-    pub event_cid: Cid,
-    pub init: ConclusionInit,
-    // TODO : rethink this, add a checkpoint to make it work in datafusion query
-    pub previous: Vec<Cid>,
-    pub data: Vec<u8>,
+    /// Index of the event. See [`ConclusionEvent`] for invariants about the index.
     pub index: u64,
+    /// The CID of the event itself. Can be used as a unique identifier for the event.
+    pub event_cid: Cid,
+    /// The stream metadata of the event.
+    pub init: ConclusionInit,
+    /// Ordered list of previous events this event references.
+    pub previous: Vec<Cid>,
+    /// Raw bytes of the event data encoded as dag-json.
+    pub data: Vec<u8>,
 }
 
-pub type CeramicTime = chrono::DateTime<chrono::Utc>;
-
+/// ConclusionTime represents a Ceramic event that contains time relevant information.
+///
+/// Additionally we have concluded to which stream the event belongs and its associated metadata.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ConclusionTime {
-    // WIP : Need clearer structure for time events
-    pub event_cid: Cid,
-    pub init: ConclusionInit,
-    pub previous: Vec<Cid>,
+    /// Index of the event. See [`ConclusionEvent`] for invariants about the index.
     pub index: u64,
+    /// The CID of the event itself. Can be used as a unique identifier for the event.
+    pub event_cid: Cid,
+    /// The stream metadata of the event.
+    pub init: ConclusionInit,
+    /// Ordered list of previous events this event references.
+    pub previous: Vec<Cid>,
+    //TODO Add temporal conclusions, i.e the block timestamp of this event
 }
 
 impl TryFrom<unvalidated::Event<Ipld>> for ConclusionInit {
