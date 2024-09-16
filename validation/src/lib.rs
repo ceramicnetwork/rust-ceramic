bce6471288a4497017cc8f2a55a91264504eada4
David Estes <5317198+dav1do@users.noreply.github.com>
2024-09-16 11:15:46 -0600

    feat: rpc client for eth blockchains (#520)
    * feat: add eth rpc client
    
    * chore: review comments
    
    * chore: improve doc comments from review feedback

--- validation/src/lib.rs
+++ validation/src/lib.rs
@@ -1,8 +1,10 @@
+mod blockchain;
 pub mod signature;
 pub mod siwx_message;
 #[cfg(test)]
 mod test;
 mod verifier;
 
+pub use blockchain::eth_rpc;
 pub use ceramic_event::unvalidated::signed::cacao;
 pub use verifier::{cacao_verifier, event_verifier, opts::VerifyCacaoOpts};
