2b9c6a06e3ed65cbeab065d920170857f2eac71a
Aaron D Goldman <goldmanaaron@gmail.com>
2024-09-23 11:44:35 -0700

    split the migraitons
    So that we don't modifiy a migration on main.

--- migrations/sqlite/20240809205108_event_init_cid_informant_type.down.sql
+++ migrations/sqlite/20240809205108_event_init_cid_informant_type.down.sql
@@ -1,6 +1,4 @@
 -- Add down migration script here
 
 -- Remove the newly added init_cid, informant, type columns
-ALTER TABLE ceramic_one_event DROP COLUMN init_cid;
-ALTER TABLE ceramic_one_event DROP COLUMN informant;
 ALTER TABLE ceramic_one_event DROP COLUMN is_time_event;
