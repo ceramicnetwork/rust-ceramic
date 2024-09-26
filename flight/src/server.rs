1883d594488b036766262988793225cef0763cba
Nathaniel Cook <nvcook42@gmail.com>
2024-09-26 10:15:13 -0600

    feat: protect FlightSQL server with sql options.
    Disables all types of SQL queries except read only queries.

--- flight/src/server.rs
+++ flight/src/server.rs
@@ -10,7 +10,7 @@ use datafusion::catalog::{Session, TableProvider};
 use datafusion::common::exec_datafusion_err;
 use datafusion::datasource::TableType;
 use datafusion::execution::config::SessionConfig;
-use datafusion::execution::context::SessionContext;
+use datafusion::execution::context::{SQLOptions, SessionContext};
 use datafusion::logical_expr::{Expr, TableProviderFilterPushDown};
 use datafusion::physical_expr::EquivalenceProperties;
 use datafusion::physical_plan::stream::RecordBatchStreamAdapter;
@@ -42,7 +42,15 @@ pub fn new_server(
         SessionConfig::new().with_default_catalog_and_schema("ceramic", "v0"),
     );
     ctx.register_table("conclusion_feed", Arc::new(FeedTable::new(feed)))?;
-    let svc = FlightServiceServer::new(FlightSqlService::new(ctx.state()));
+    let svc = FlightServiceServer::new(
+        FlightSqlService::new(ctx.state()).with_sql_options(Some(
+            // Disable all access except read only queries.
+            SQLOptions::new()
+                .with_allow_dml(false)
+                .with_allow_ddl(false)
+                .with_allow_statements(false),
+        )),
+    );
     Ok(Server::builder().add_service(svc))
 }
 
