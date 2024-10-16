33f9ee93d208f9af66c8ca2ba26f7ec87d762dae
Nathaniel Cook <nvcook42@gmail.com>
2024-10-16 09:44:52 -0600

    feat: add query command to ceramic-one (#545)
    * feat: add query command to ceramic-one
    
    * feat: teach query command how to read the tables
    
    There are currently two tables in the datafusion _pipeline_
    architecture, conclucsion_feed and doc_state.
    
    This change moves the initialization code for datafusion to interact
    with those tables into a new ceramic-pipeline crate where both the olap
    and query commands can reuse the logic. This new crate can be a home for
    future work in this _pipeline_ architecture as we flesh it out. For
    example we will likely move the aggregator/ceramic_patch logic into this crate.
    
    In either case the query command now knows how to query both tables when
    it first starts up. This table access is via flight sql and direct
    object store access.
    
    * feat: expose cid_string udf in query and flight sql
    
    * fix: tests
    
    * fix: use better names for cli args

---
+++ one/src/query.rs
@@ -0,0 +1,56 @@
+use clap::Args;
+use datafusion_cli::{exec::exec_from_repl, print_options::PrintOptions};
+use object_store::aws::AmazonS3Builder;
+
+#[derive(Args, Debug)]
+pub struct QueryOpts {
+    /// Query endpoint of the ceramic one daemon.
+    #[arg(
+        short,
+        long,
+        default_value = "http://127.0.0.1:5102",
+        env = "CERAMIC_ONE_QUERY_ENDPOINT"
+    )]
+    query_endpoint: String,
+
+    /// S3 bucket name.
+    /// When configured the aggregator will support storing data in S3 compatible object stores.
+    ///
+    /// Credentials are read from the environment:
+    ///
+    ///   * AWS_ACCESS_KEY_ID -> access_key_id
+    ///   * AWS_SECRET_ACCESS_KEY -> secret_access_key
+    ///   * AWS_DEFAULT_REGION -> region
+    ///   * AWS_ENDPOINT -> endpoint
+    ///   * AWS_SESSION_TOKEN -> token
+    ///   * AWS_ALLOW_HTTP -> set to "true" to permit HTTP connections without TLS
+    ///
+    #[arg(long, env = "CERAMIC_ONE_AWS_BUCKET")]
+    s3_bucket: String,
+}
+
+pub async fn run(opts: QueryOpts) -> anyhow::Result<()> {
+    let mut ctx = ceramic_pipeline::session_from_config(opts).await?;
+
+    datafusion_functions_json::register_all(&mut ctx)?;
+
+    let mut print_options = PrintOptions {
+        format: datafusion_cli::print_format::PrintFormat::Automatic,
+        quiet: false,
+        maxrows: datafusion_cli::print_options::MaxRows::Unlimited,
+        color: true,
+    };
+
+    exec_from_repl(&ctx, &mut print_options).await.unwrap();
+
+    Ok(())
+}
+
+impl From<QueryOpts> for ceramic_pipeline::Config {
+    fn from(value: QueryOpts) -> Self {
+        Self {
+            flight_sql_endpoint: value.query_endpoint,
+            aws_s3_builder: AmazonS3Builder::from_env().with_bucket_name(&value.s3_bucket),
+        }
+    }
+}
