00a919ef990e694867d50d0320cadf4edc1b01f8
Github Automation <github@3box.io>
2024-12-20 20:19:59 +0000

    chore: version v0.47.2

--- kubo-rpc-server/src/lib.rs
+++ kubo-rpc-server/src/lib.rs
@@ -21,7 +21,7 @@ use swagger::{ApiError, ContextWrapper};
 type ServiceError = Box<dyn Error + Send + Sync + 'static>;
 
 pub const BASE_PATH: &str = "/api/v0";
-pub const API_VERSION: &str = "0.47.1";
+pub const API_VERSION: &str = "0.47.2";
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[must_use]
