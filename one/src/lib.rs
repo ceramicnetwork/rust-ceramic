f8f884d502f2a99deee1cbed630162b6b4a291c9
Nathaniel Cook <nvcook42@gmail.com>
2025-03-18 11:03:13 -0600

    fix: use new default gnosis rpc endpoint (#683)

--- one/src/lib.rs
+++ one/src/lib.rs
@@ -127,8 +127,8 @@ impl Network {
                 anyhow::bail!("no Ethereum RPC URLs specified for Mainnet")
             }
             Network::TestnetClay => {
-                info!("no Ethereum RPC URLs specified for Clay Testnet, defaulting to https://rpc.ankr.com/gnosis");
-                Ok(vec!["https://rpc.ankr.com/gnosis".to_string()])
+                info!("no Ethereum RPC URLs specified for Clay Testnet, defaulting to https://rpc.gnosis.gateway.fm/");
+                Ok(vec!["https://rpc.gnosis.gateway.fm/".to_string()])
             }
             Network::DevUnstable => {
                 info!("no Ethereum RPC URLs specified for dev-unstable network, defaulting to https://ethereum-sepolia-rpc.publicnode.com");
