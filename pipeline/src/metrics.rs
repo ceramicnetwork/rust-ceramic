cbc04d67c33945604ef79c67fa949c5800d7b4a8
Nathaniel Cook <nvcook42@gmail.com>
2025-03-19 14:55:56 -0600

    wip: adds scaffold for resolver actor

--- pipeline/src/metrics.rs
+++ pipeline/src/metrics.rs
@@ -11,14 +11,23 @@ use prometheus_client::{
 pub struct Metrics {
     pub(crate) message_count: Family<MessageLabels, Counter>,
 
+    pub(crate) concluder_poll_new_events_loop_count: Counter,
+
     pub(crate) aggregator_new_conclusion_events_count: Counter,
 
-    pub(crate) concluder_poll_new_events_loop_count: Counter,
+    pub(crate) resolver_new_event_states_count: Counter,
 }
 impl Metrics {
     /// Register and construct Metrics
     pub fn register(registry: &mut Registry) -> Self {
         let sub_registry = registry.sub_registry_with_prefix("pipeline");
+
+        register!(
+            concluder_poll_new_events_loop_count,
+            "Number of times the loop to poll new conclusion events has run",
+            Counter::default(),
+            sub_registry
+        );
 
         register!(
             message_count,
@@ -32,18 +41,18 @@ impl Metrics {
             Counter::default(),
             sub_registry
         );
-
         register!(
-            concluder_poll_new_events_loop_count,
-            "Number of times the loop to poll new conclusion events has run",
+            resolver_new_event_states_count,
+            "Number of new event states delivered to the resolver",
             Counter::default(),
             sub_registry
         );
 
         Self {
             message_count,
-            aggregator_new_conclusion_events_count,
             concluder_poll_new_events_loop_count,
+            aggregator_new_conclusion_events_count,
+            resolver_new_event_states_count,
         }
     }
 }
